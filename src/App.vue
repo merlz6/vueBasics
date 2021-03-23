94ec3646c7af7b134c173ddcf8a21b7f3293669a
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2021-03-22 21:48:07 -0400

    vue props and components beginner, toggle

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-<h1>Hello</h1>
+  <div>
+    <h1>Hello</h1>
+    <h2>{{cheese}}</h2>
+    <HelloWorld  v-bind:cheese="cheese"/>
+    <button v-on:click="handleClick">Click Me</button>
+
+
+  </div>
+
 </template>
 
 
 <script>
+import HelloWorld from "./components/HelloWorld.vue"
+
+
+
 export default {
-  name:'App'
+  name: 'App',
+  data:function(){
+    return{
+        cheese:"gouda"
+    }
+  },
+  components:{HelloWorld},
+  methods:{
+    handleClick:function(){
+      this.cheese = "munster"
+    }
+  }
 }
 </script>
 
 
 <style>
 h1 {
-color:red;
+  color: red;
 }
 </style>
